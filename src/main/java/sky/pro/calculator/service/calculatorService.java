8a5d48f53ab1d9c9f5dc1bc21f7df8150c5e98ff
Andrey <andrey.volk.1999@gmail.com>
2024-10-06 23:30:03 +1000

    add Solution Calculator and Test JUnit

---
+++ src/main/java/sky/pro/calculator/service/calculatorService.java
@@ -0,0 +1,12 @@
+package sky.pro.calculator.service;
+
+
+
+
+public interface calculatorService {
+    String getHello();
+    Integer plus(Integer num1,Integer num2);
+    Integer minus(Integer num1,Integer num2);
+    Integer multiply(Integer num1,Integer num2);
+    Integer divide(Integer num1,Integer num2);
+}
